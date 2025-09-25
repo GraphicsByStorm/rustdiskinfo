ff702dce3ec25d203848a577d2705d0e63b90dcb
Stormy Wentworth <business@graphicsbystorm.com>
2025-09-24 21:58:53 -0400

    Added basic ui.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod tools;
+mod ui;
+
+use crossterm::{execute, terminal::{enable_raw_mode, disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}};
+use ratatui::{backend::{CrosstermBackend}, Terminal};
+
+fn main() -> Result<(), std::io::Error> {
+    enable_raw_mode()?;
+    execute!(std::io::stdout(), EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(std::io::stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        ui::show_menu(f);
+    })?;
+
+    disable_raw_mode()?;
+    execute!(std::io::stdout(), LeaveAlternateScreen)?;
+
+    Ok(())
 }
